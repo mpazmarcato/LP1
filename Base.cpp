d1447d344177cd646d25475d95d3f08ecf543321
MariaPaz04 <mpazmarcato@gmail.com>
2024-03-12 16:53:25 -0300

    First class example

--- Base.cpp
+++ Base.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
 
+//define IDADE_MINIMA 18,
+//using std::cin, std::cout, std::endl;
+using namespace std;
+
+int main(){
+    int x;
+
+    cout << "Digite um número: " << endl;
+    cin >> x;
+
+    if (x >= 18)
+    {
+        cout << "Você é velho" << endl;  
+    } else {
+        cout << "Você é novo" << endl;
+    }
+    
+    return 0;
+}
